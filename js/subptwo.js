440fcb56b6003920092e224b7ea804e4ace1897c
luben <lb712@163.com>
2017-09-01 14:47:36 +0800

    one

---
+++ js/subptwo.js
@@ -0,0 +1,12 @@
+// $(".jij").mouseenter(function(){
+// 			$(this).css("background","#000");
+			
+
+// });
+//上面是测试
+$(".ilclk").mousedown(function(){
+		$(this).css({border:"3px solid red"});
+
+
+});
+		
---
+++ php/luben3.php
@@ -0,0 +1,15 @@
+<?php
+		header("Content-type:text/html;charset=utf-8");
+		$name=$_POST['userName'];
+
+		$pass=$_POST['password'];
+
+		if($name=="18691004316" ){
+			echo "0";
+
+		}else{
+			echo "1";
+		}
+
+
+?>
